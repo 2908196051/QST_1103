ccc8f3fcd426c226a2ec2a9e17d236ff7c56ed49
2908196051 <949195943@qq.com>
2016-11-03 18:36:50 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,24 @@
-package No4;
+package GID.AID;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ReducerOfIPFilter {
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Set<String> set = new HashSet<String>();
+		// List<String> list = new ArrayList<String>();
+		while (sc.hasNext()) {
+			String line = sc.nextLine();
+			String[] tokens = line.split("\t");
+			set.add(tokens[0]);
 		}
+		System.out.println(set.size());
+
+		sc.close();
+
 	}
 }
